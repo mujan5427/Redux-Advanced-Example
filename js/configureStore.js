eeca933d778f4aac0380bbfad9b4a97b1bb525f9
Justin Ho <mujan5427@gmail.com>
2017-05-19 20:25:26 +0800

    added a few comments

--- js/configureStore.js
+++ js/configureStore.js
@@ -3,6 +3,9 @@ import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers'
 
 export default function configureStore(preloadedState) {
+
+  // When a store is created,
+  // Redux dispatches a dummy action to your reducer to populate the store with the initial state.
   return createStore(
     rootReducer,
     preloadedState,
